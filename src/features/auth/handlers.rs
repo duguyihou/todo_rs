34112bbd12d9233e0b8f1a332323891e8a9bbbaa
duguyihou <duguyihou@gmail.com>
2025-03-19 20:35:24 +1100

    feat: register (#16)
    * chore: wip
    
    * feat: wip
    
    * feat: register

--- src/features/auth/handlers.rs
+++ src/features/auth/handlers.rs
@@ -1,11 +1,31 @@
-use super::models::{LoginRequest, RegisterRequest};
+use super::models::{AuthCredentialsDto, AuthError};
 use axum::{extract::State, Json};
+use sqlx::PgPool;
 
-pub async fn login(Json(payload): Json<LoginRequest>) -> &'static str {
+pub async fn login() -> &'static str {
     "Login successful"
 }
 
-pub async fn register(Json(payload): Json<RegisterRequest>) -> &'static str {
-    // Handle registration logic
-    "Registration successful"
+pub async fn register(
+    State(pool): State<PgPool>,
+    Json(auth_credentials_dto): Json<AuthCredentialsDto>,
+) -> Result<Json<String>, AuthError> {
+    let AuthCredentialsDto { email, password } = auth_credentials_dto;
+    let created_at = chrono::Utc::now();
+
+    sqlx::query!(
+        r#"
+        INSERT INTO users (email, password, created_at)
+        VALUES ($1, $2, $3)
+        RETURNING id, email, password, created_at
+        "#,
+        email,
+        password,
+        created_at
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|_| AuthError::MissingCredentials)?;
+
+    Ok(Json(format!("User registered successfully")))
 }
--- src/features/auth/models.rs
+++ src/features/auth/models.rs
@@ -1,14 +1,48 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+use sqlx::prelude::FromRow;
 
-#[derive(Deserialize, Serialize)]
-pub struct LoginRequest {
-    pub username: String,
+#[derive(Debug, Serialize, Deserialize, Clone, FromRow)]
+pub struct User {
+    pub id: i32,
+    pub email: String,
+    pub password: String,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct AuthCredentialsDto {
+    pub email: String,
     pub password: String,
 }
 
-#[derive(Deserialize)]
-pub struct RegisterRequest {
-    pub username: String,
-    pub email: String,
-    pub password: String,
+#[derive(Debug, Serialize, Deserialize)]
+pub enum AuthError {
+    WrongCredentials,
+    MissingCredentials,
+    TokenCreationError,
+    InvalidToken,
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "wrong credentials"),
+            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing crdentials"),
+            AuthError::TokenCreationError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error")
+            }
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        };
+        let body = Json(json!({
+            "error": error_message
+        }));
+        (status, body).into_response()
+    }
 }
